3631ec7f5fb9e3a06cc218352f121b8d4e5cfeec
hburdge1 <95104319+hburdge1@users.noreply.github.com>
2022-01-18 08:36:38 -0800

    finished

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { UserName } from "src/data/user.js"
+import { city} from "src/data/user.js"
 function Home() {
   return (
     <div id="home">
--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,23 @@
+import React from "react";
+
 const username = "Liza";
 const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+export default class UserName extends React.Component{
+render() {
+    return username
+}
+}
+export default class City extends React.Component{
+   render() {
+    return city
+} 
+}
+export default class Image extends React.Component{
+    render() {
+    return image
+}
+}
